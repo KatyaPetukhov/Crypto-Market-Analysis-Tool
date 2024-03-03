e9b7d67741b1eafccec7d6ecaa803763f1bf4938
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-03-03 23:32:46 +0200

    added rating scale and added rates.js

---
+++ Frontend/rates.js
@@ -0,0 +1,14 @@
+// JavaScript to update the tooltip value
+
+
+document.addEventListener('DOMContentLoaded', () => {
+const circles = document.getElementById('rate_circles');
+const labels = document.getElementById('rate_labels');
+const cirle = circles.children[2].children[0];
+console.log(cirle);
+
+
+cirle.classList.remove("hidden");
+
+
+});
