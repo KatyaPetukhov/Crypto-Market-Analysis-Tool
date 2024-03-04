b80025a065a68dcacee4d2b7d2728cea0fca5fac
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-03-04 10:44:29 +0200

    added random func that creates predictions

--- Frontend/rates.js
+++ Frontend/rates.js
@@ -1,10 +1,14 @@
 // JavaScript to update the tooltip value
 
-
+//"Black Box" function, not implemented yet. 
+//This block needs to get wallets history info and build bitcion predictions according
+// to this info by AI. 
+//On this level we don't do implemention to this, so it creates random prediction.
+ 
 document.addEventListener('DOMContentLoaded', () => {
 const circles = document.getElementById('rate_circles');
 const labels = document.getElementById('rate_labels');
-const cirle = circles.children[2].children[0];
+const cirle = circles.children[getRandomNumber()].children[0];
 console.log(cirle);
 
 
@@ -12,3 +16,7 @@ cirle.classList.remove("hidden");
 
 
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 5);
+}
