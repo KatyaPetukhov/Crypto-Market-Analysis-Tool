6e948417293f60ed4ff614d91d7c680f1649074d
hadiDanial <hadiDanial@users.noreply.github.com>
2024-03-06 19:55:57 +0200

    Add loading to chart and disable update button when no dates are selected

--- Frontend/charts.js
+++ Frontend/charts.js
@@ -1,66 +1,96 @@
 
 let chart = null;
-
+let loading;
 document.addEventListener('DOMContentLoaded', () => {
     const BACKEND_URL = 'https://crypto-market-analysis-tool-f12d66bb7184.herokuapp.com'
+    loading = document.getElementById('loading');
+    loading.classList.remove('invisible');
+    
     fetch(`${BACKEND_URL}/get-bitcoin-history`)
-      .then((response) => response.json())
-      .then((json) => createChart(json))
-      .catch((err) => console.log(err))
-
+    .then((response) => response.json())
+    .then((json) => createChart(json))
+    .catch((err) => console.log(err))
+    
     const updateButton = document.getElementById('updateDatesBtn');
     const fromDateInput = document.getElementById('fromDateInput');
     const untilDateInput = document.getElementById('untilDateInput');
+    setUpdateButtonDisabled(updateButton, true);
     updateButton.onclick = () => {
       const fromDate = new Date(fromDateInput.value);
       const untilDate = new Date(untilDateInput.value);
       const fromTimestamp = Math.floor(fromDate.getTime() / 1000);
       const untilTimestamp = Math.floor(untilDate.getTime() / 1000);
+      loading.classList.remove('invisible');
       fetch(`${BACKEND_URL}/get-bitcoin-history?from=${fromTimestamp}&until=${untilTimestamp}`)
-        .then((response) => response.json())
-        .then((json) => createChart(json))
-        .catch((err) => console.log(err))
+      .then((response) => response.json())
+      .then((json) => createChart(json))
+      .catch((err) => console.log(err))
     };
     fromDateInput.onblur = () => {
-      console.log(fromDateInput.value);
+      // console.log(fromDateInput.value);
       let fromDate = new Date(fromDateInput.value);
       let untilDate = new Date(untilDateInput.value);
       let now = new Date();
       let minDate = new Date('2014-09-17');
-  
+      if(fromDate === undefined) {
+        setUpdateButtonDisabled(updateButton, true);
+        return;
+      }
+      else if(untilDate !== undefined && fromDate !== undefined) {
+        setUpdateButtonDisabled(updateButton, false);        
+      }
+      
       if (fromDate > untilDate) {
-          untilDateInput.value = fromDateInput.value;
+        untilDateInput.value = fromDateInput.value;
       }
       else if (fromDate > now) {
-          fromDateInput.value = now.toISOString().split('T')[0];
+        fromDateInput.value = now.toISOString().split('T')[0];
       }
       else if (fromDate < minDate) {
           fromDateInput.value = minDate.toISOString().split('T')[0];
-      }
-  };
-  
-  untilDateInput.onblur = () => {
-      console.log(untilDateInput.value);
-      let fromDate = new Date(fromDateInput.value);
-      let untilDate = new Date(untilDateInput.value);
-      let now = new Date();
-      let minDate = new Date('2014-09-17');
-  
-      if (untilDate < fromDate) {
+        }
+      };
+      
+      untilDateInput.onblur = () => {
+        // console.log(untilDateInput.value);
+        let fromDate = new Date(fromDateInput.value);
+        let untilDate = new Date(untilDateInput.value);
+        let now = new Date();
+        let minDate = new Date('2014-09-17');
+        if(untilDate === undefined) {
+          setUpdateButtonDisabled(updateButton, true);
+          return;
+        }
+        else if(untilDate !== undefined && fromDate !== undefined) {
+          setUpdateButtonDisabled(updateButton, false);                
+        }
+        
+        if (untilDate < fromDate) {
           untilDateInput.value = fromDateInput.value;
-      }
-      else if (untilDate > now) {
+        }
+        else if (untilDate > now) {
           untilDateInput.value = now.toISOString().split('T')[0];
-      }
-      else if (untilDate < minDate) {
+        }
+        else if (untilDate < minDate) {
           untilDateInput.value = minDate.toISOString().split('T')[0];
       }
   };
   });
 
-  
+const setUpdateButtonDisabled = (btn, isDisabled) => {
+  btn.disabled = isDisabled;
+  if (isDisabled) {
+    btn.classList.remove('bg-blue-500');
+    btn.classList.add('bg-gray-300');
+  }
+  else {
+    btn.classList.remove('bg-gray-300');
+    btn.classList.add('bg-blue-500');
+  }
+}
 
 const createChart = (data) => {
+  loading.classList.add('invisible');
   const dataInfo = data.map((row) => parseFloat(row[4].replace(',', ''))).reverse();
   console.log(data.toReversed());
   data.reverse();
