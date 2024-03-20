185fc6a0fb725325c0ebb0ff44b5fecd2599414e
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-03-20 21:14:09 +0200

    Added extra styles

--- frontend_react/src/components/Button.tsx
+++ frontend_react/src/components/Button.tsx
@@ -3,14 +3,19 @@ interface ButtonProps {
   name: string;
   onClick: () => void;
   disabled?: boolean;
+  extraStyles?: string;
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
+  const extraStyles = props.extraStyles || "";
   return (
     <button
       //   id="updateDatesBtn"
       disabled={props.disabled || false}
-      className="btn h-10 px-5 w-full bg-blue-500 hover:bg-blue-700 text-white block p-2 rounded-lg disabled:bg-slate-300"
+      className={
+        "btn h-10 px-5 w-full bg-blue-500 hover:bg-blue-700 text-white block p-2 rounded-lg disabled:bg-slate-300 " +
+        extraStyles
+      }
       onClick={props.onClick}
     >
       {props.name}
