389c5f73a2a74fc78579235651f4d0e74e423055
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-03-20 21:15:22 +0200

    Moved files

--- frontend_react/src/features/Chart.tsx
+++ frontend_react/src/features/Chart.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Button from "./Button";
-import InputDate from "./InputDate";
-import Loading from "./Loading";
+import Button from "../components/Button";
+import InputDate from "../components/InputDate";
+import Loading from "../components/Loading";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -100,42 +100,40 @@ const Chart = () => {
   }, [data]);
 
   return (
-    <main className="container mx-auto px-6 py-8 ">
-      <section className="mx-auto">
-        <h2 className="text-2xl font-bold text-gray-800 my-6">
-          Explore the Crypto World
-        </h2>
-        <div className="mx-auto flex flex-wrap items-end gap-6 mb-6">
-          <div>
-            From:
-            <InputDate setDate={setFromDate} maxDate={untilDate} />
-          </div>
-
-          <div>
-            Until:
-            <InputDate setDate={setUntilDate} minDate={fromDate} />
-          </div>
-          <div>
-            <Button
-              name={"Update Data"}
-              disabled={fromDate === undefined || untilDate === undefined}
-              onClick={() => {
-                if (fromDate === undefined || untilDate === undefined) return;
-                setApiArg({
-                  from: Math.floor(fromDate.getTime() / 1000).toString(),
-                  until: Math.floor(untilDate.getTime() / 1000).toString(),
-                });
-                setTimeout(() => {
-                  refetch();
-                }, 10);
-              }}
-            ></Button>
-          </div>
+    <section className="container mx-auto px-6 py-8">
+      <h2 className="text-2xl font-bold text-gray-800 my-6">
+        Explore the Crypto World
+      </h2>
+      <div className="mx-auto flex flex-wrap items-end gap-6 mb-6">
+        <div>
+          From:
+          <InputDate setDate={setFromDate} maxDate={untilDate} />
         </div>
-        <Loading isLoading={isLoading || isFetching} />
-        <Line options={options} data={chartData} />
-      </section>
-    </main>
+
+        <div>
+          Until:
+          <InputDate setDate={setUntilDate} minDate={fromDate} />
+        </div>
+        <div>
+          <Button
+            name={"Update Data"}
+            disabled={fromDate === undefined || untilDate === undefined}
+            onClick={() => {
+              if (fromDate === undefined || untilDate === undefined) return;
+              setApiArg({
+                from: Math.floor(fromDate.getTime() / 1000).toString(),
+                until: Math.floor(untilDate.getTime() / 1000).toString(),
+              });
+              setTimeout(() => {
+                refetch();
+              }, 10);
+            }}
+          ></Button>
+        </div>
+      </div>
+      <Loading isLoading={isLoading || isFetching} />
+      <Line options={options} data={chartData} />
+    </section>
   );
 };
 
