1c51384576c5d9c698e1611c6fa4af207b5bc291
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-03-20 17:05:25 +0200

    Added disabled state to a button

--- frontend_react/src/components/Button.tsx
+++ frontend_react/src/components/Button.tsx
@@ -2,13 +2,15 @@ import React from "react";
 interface ButtonProps {
   name: string;
   onClick: () => void;
+  disabled?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
   return (
     <button
       //   id="updateDatesBtn"
-      className="btn h-10 px-5 w-full bg-blue-500 hover:bg-blue-700 text-white block p-2 rounded-lg"
+      disabled={props.disabled || false}
+      className="btn h-10 px-5 w-full bg-blue-500 hover:bg-blue-700 text-white block p-2 rounded-lg disabled:bg-slate-300"
       onClick={props.onClick}
     >
       {props.name}
