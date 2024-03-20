cc14bb916ac7604591bad2e35cf7b66ae78a55ea
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-03-20 21:13:16 +0200

    Added routing

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
+import Prediction from "./features/Prediction";
+import Header from "./components/Header";
+import Wallet from "./screens/Wallet";
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <Router>
+      <Header />
+      <div className="bg-gray-100">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/wallet" element={<Wallet />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
