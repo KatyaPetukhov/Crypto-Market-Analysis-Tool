d9d2039da058506e2e619ea28133627e0eec9246
hadiDanial <hadiDanial@users.noreply.github.com>
2024-03-28 22:06:07 +0200

    BrowserRouter to HashRouter

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
-import Prediction from "./features/Prediction";
 import Header from "./components/Header";
 import Wallet from "./screens/Wallet";
 import Footer from "./components/Footer";
