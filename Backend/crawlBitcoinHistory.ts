ef90a8dddd1737b61ae536d7ec27672f80644a65
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-08-25 00:33:56 +0300

    added read csv files version for testing

--- Backend/crawlBitcoinHistory.ts
+++ Backend/crawlBitcoinHistory.ts
@@ -2,56 +2,66 @@
 // Using the crawler NPM package.
 import { BitcoinHistory } from "./types";
 import Papa from "papaparse";
+const fs = require("fs");
 
 // Get the bitcoin history data from Yahoo Finance as a CSV file, parse the result and return it.
 // If the from and until are provided we use them, otherwise we use default values.
 const crawlBitcoinHistory = async (
-    from?: number,
-    until?: number,
-    forceOneDay?: boolean
+  from?: number,
+  until?: number,
+  forceOneDay?: boolean
 ): Promise<BitcoinHistory[]> => {
-    let isSpecificPeriod = false;
-    if (from && until) {
-        isSpecificPeriod = true;
+  let isSpecificPeriod = false;
+  if (from && until) {
+    isSpecificPeriod = true;
+  }
+  const minimum = 2678400;
+  const maximum = 298512000;
+  const currentDate = new Date();
+  const oneYearAgo = new Date(
+    currentDate.getFullYear() - 1,
+    currentDate.getMonth(),
+    currentDate.getDate()
+  );
+  const fromTimestamp = Math.floor(oneYearAgo.getTime() / 1000);
+  const untilTimestamp = Math.floor(currentDate.getTime() / 1000);
+  from = from || fromTimestamp;
+  until = until || untilTimestamp;
+  const value = isSpecificPeriod
+    ? (until || maximum) - (from || minimum)
+    : minimum;
+
+  //  The API allows us to change the amount of points we get, either one point per day, one per week, one per month.
+  //  We choose based on the amount of time between the dates.
+
+  let interval = "1d";
+  if (forceOneDay === undefined || forceOneDay === false) {
+    if (value > 63158400) {
+      interval = "1mo";
+    } else if (!isSpecificPeriod || value > 8035200) {
+      interval = "1wk";
     }
-    const minimum = 2678400;
-    const maximum = 298512000;
-    const currentDate = new Date();
-    const oneYearAgo = new Date(
-        currentDate.getFullYear() - 1,
-        currentDate.getMonth(),
-        currentDate.getDate()
-    );
-    const fromTimestamp = Math.floor(oneYearAgo.getTime() / 1000);
-    const untilTimestamp = Math.floor(currentDate.getTime() / 1000);
-    from = from || fromTimestamp;
-    until = until || untilTimestamp;
-    const value = isSpecificPeriod
-        ? (until || maximum) - (from || minimum)
-        : minimum;
+  }
 
-    //  The API allows us to change the amount of points we get, either one point per day, one per week, one per month.
-    //  We choose based on the amount of time between the dates.
+  // The URL to download the data as CSV file.
 
-    let interval = "1d";
-    if (forceOneDay === undefined || forceOneDay === false) {
-        if (value > 63158400) {
-            interval = "1mo";
-        } else if (!isSpecificPeriod || value > 8035200) {
-            interval = "1wk";
-        }
-    }
+  const yahooDownloadURL = `https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=${from}&period2=${until}&interval=${interval}&events=history&includeAdjustedClose=true`;
+  const result = await fetch(yahooDownloadURL);
+  const data = await result.text();
 
-    // The URL to download the data as CSV file.
+  // Use papa parse to parse the CSV file into bitcion history array.
 
-    const yahooDownloadURL = `https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=${from}&period2=${until}&interval=${interval}&events=history&includeAdjustedClose=true`;
-    const result = await fetch(yahooDownloadURL);
-    const data = await result.text();
-
-    // Use papa parse to parse the CSV file into bitcion history array.
-
-    const parsedCSV = Papa.parse<BitcoinHistory>(data, { header: true });
-    return parsedCSV.data;
+  const parsedCSV = Papa.parse<BitcoinHistory>(data, { header: true });
+  return parsedCSV.data;
 };
 
-export { crawlBitcoinHistory };
+function readBitcoinHistory(fileName: string) {
+  const csvFile = fs.readFileSync(fileName, "utf8");
+  const parsedCSV = Papa.parse<BitcoinHistory>(csvFile, {
+    header: true,
+    delimiter: "|",
+  });
+  return parsedCSV.data;
+}
+
+export { crawlBitcoinHistory, readBitcoinHistory };
