f1e43e6d26497d72df65b7e0ec05c480a9c13d8f
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-09-06 13:26:46 +0300

    Updated URLs for heroku

--- frontend_react/src/redux/emptyApi.ts
+++ frontend_react/src/redux/emptyApi.ts
@@ -1,13 +1,13 @@
 // Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-// Import { REACT_APP_BASE_URL } from "@env"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+//Import { REACT_APP_BASE_URL } from "@env"
 const url = "https://crypto-market-analysis-tool-f12d66bb7184.herokuapp.com/";
+//const url = "http://localhost:3001/";
+
 console.log(url);
 
 // Initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: url, 
-    
-    }),
+  baseQuery: fetchBaseQuery({ baseUrl: url }),
   endpoints: () => ({}),
-})
+});
